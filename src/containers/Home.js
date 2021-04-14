b1e6360218be20ff9b1bde03281755a778ac2235
janthony1407 <73077332+janthony1407@users.noreply.github.com>
2021-04-14 18:30:31 -0500

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -82,7 +82,7 @@ export default class Home extends Component {
   renderNotes() {
     return (
       <div className="notes">
-        <PageHeader>Your Notes</PageHeader>
+        <PageHeader>Productos</PageHeader>
         <ListGroup>
           {!this.state.isLoading && this.renderNotesList(this.state.notes)}
         </ListGroup>
